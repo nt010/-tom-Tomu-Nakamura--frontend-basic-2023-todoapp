557d8bc2abfbf8add61665a20a93d4eb55839ecc
nkmr <tomnkmr0120@gmail.com>
2023-11-09 15:54:58 +0900

    Atoms/EditButtonの作成

---
+++ src/components/Atoms/EditButton/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import pencil from "../../../assets/svg/pencil.svg";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+
+export const EditButton = (props) => {
+    return (
+    <StyledButton onClick={props.onClick}>
+        <img src={pencil}/>
+    </StyledButton>
+    );
+};
+
+export default EditButton;
+
+const StyledButton = styled.button`
+    display: flex;
+    width: 20px;
+    height: 20px;
+    flex-shrink: 0;
+    background: none;
+    border: none;
+    border-radius: 50%;
+    justify-content: center;
+    transition: 0.3s;
+    img{
+        display: flex;
+        width: 20px;
+        height: 20px;
+        
+    }
+    :hover{
+        cursor: pointer;
+        background-color: ${COLOR.LIGHT_GRAY_TRANSLUCENT};
+    }
+`;
