85f6d9daa53204210796f8c5d1adfc0d2db09632
nkmr <tomnkmr0120@gmail.com>
2023-11-13 14:46:52 +0900

    TaskButtonの作成

---
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import plus from "../../../assets/svg/plus.svg";
+import styled from "styled-components";
+import FONT from "../../../variables/font_family";
+import COLOR from "../../../variables/color";
+import TEXT from "../../../variables/texts";
+
+export const TaskButton = (props) =>{
+    return (
+        <StyledTaskButton onClick={props.onClick}>
+            <img src={plus} className="img-plus"/>
+            <p className="taskLabel">タスクを追加</p>
+        </StyledTaskButton>
+    );
+};
+
+const StyledTaskButton = styled.button`
+    display: flex;
+    padding: 2px 6px;
+    align-items: center;
+    gap: 10px;
+    background: none;
+    border: none;
+    border-radius: 14px;
+    transition: 0.3s;
+
+    .img-plus{
+        width: 20px;
+        height: 20px;
+    }
+
+    .taskLabel{
+        color: ${COLOR.GREEN};
+        font-family: ${FONT.NOTO_SANS};
+        ${TEXT.S};
+        margin: 0;
+    }
+
+    :hover{
+        cursor: pointer;
+        background-color: ${COLOR.GREEN_TRANSLUCENT}
+    }
+`;
