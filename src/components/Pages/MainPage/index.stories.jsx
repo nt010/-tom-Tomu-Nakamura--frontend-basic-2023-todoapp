c25605c601f0c4dc48568e702dd8ff0d1ea0f318
nkmr <tomnkmr0120@gmail.com>
2023-12-27 18:19:45 +0900

    StorybookにAlertを表示

--- src/components/Pages/MainPage/index.stories.jsx
+++ src/components/Pages/MainPage/index.stories.jsx
@@ -1,5 +1,19 @@
+import { Template } from "webpack";
+import { AlertHandlerProvider } from "../../../contexts/alert_handler";
 import { MainPage } from "./index";
+import { AlertManager } from "../../Organisms/AlertManager";
 
 export default {component: MainPage};
 
-export const Default = {};
+export const Default = Template.bind({});
+Default.args = {};
+Default.decorators = [
+    (Stoty) => (
+        <div>
+            <AlertHandlerProvider>
+                <AlertManager/>
+                <Stoty/>
+            </AlertHandlerProvider>
+        </div>
+    )
+];
